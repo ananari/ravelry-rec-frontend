947b5eb338ec6f8353180bc5468ee0e0bc6f5b8f
Anais Ahmed <anaismahmed@uchicago.edu>
2020-05-04 16:16:30 -0500

    worked on form

---
+++ src/Testform.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+const testURL = "http://localhost:8000";
+let postObj = {
+  method: "POST",
+  headers: {
+    "X-Requested-With": "XMLHttpRequest"
+  }
+}
+
+export default class Testform extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      query: "",
+      result: ""
+    }
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.query)
+    let formData = new FormData();
+    formData.append("query", this.state.query)
+    postObj["body"] = formData
+    fetch(testURL, postObj)
+    .then(res => res.json())
+    .then(json => {this.setState(json)})
+    .catch(error => console.log(error))
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input type="textarea" name="query" value={this.state.query} onChange={event => this.handleChange(event)}/>
+          <input type="submit" />
+        </form>
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
+}
